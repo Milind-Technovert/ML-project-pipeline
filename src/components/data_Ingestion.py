f569bd7ad0341e91753f214c065b605e5455740b
Milind Mali <milind.mali@Technovert.com>
2024-05-22 13:51:18 +0530

    model trainer file is added

--- src/components/data_Ingestion.py
+++ src/components/data_Ingestion.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 from src.exception import  CustomException
 from src.logger import logging
 from data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -57,3 +58,5 @@ if __name__=="__main__":
     data_transformation=DataTransformation()
     train_arr, test_arr, _ =data_transformation.inititate_data_transformation(train_data_path,test_data_path)
 
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
